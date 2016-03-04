22ebdb5d1fea56922916605731e5bebc64032db2
ilias <ilias.hicham@gadz.org>
2016-03-04 20:10:28 +0100

    page favorites in progress / tags added

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,13 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  has_many :profile_tags
+  has_many :profile_tags, dependent: :destroy
+
+  def get_tags
+    tags = []
+    self.profile_tags.each do |tag|
+      tag.value > 0 ? (tags << Tag.find(tag.tag_id).name) : nil
+    end
+    tags
+  end
+
 end
